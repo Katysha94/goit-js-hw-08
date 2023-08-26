1322bd665ef81cc4f551c5854755a77ec2726f4c
Katysha94 <Vasilevakata76@gmail.com>
2023-08-26 20:26:10 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryList = document.querySelector(".gallery")
+
+galleryList.style.listStyle = "none"
+
+const renderList = (arr) => arr.map(({ original, preview, description }) =>
+ `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+<img
+class="gallery__image"
+src="${preview}"
+data-source="${original}"
+alt="${description}"
+/>
+</a>
+</li>`).join("");
+
+galleryList.insertAdjacentHTML("beforeend", renderList(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+const handleListClick = (event) => {
+      event.preventDefault();
+    if (event.currentTarget === event.target) {
+    return;
+}
+    const selectedImage = event.target.getAttribute('data-source')
+    
+    // const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+     lightbox.open(selectedImage);
+
+}
+
+
+
+galleryList.addEventListener('click', handleListClick)
