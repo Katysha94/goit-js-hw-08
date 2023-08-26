2385bebfcd5cfedc80ba5fda6ba6b75109d9407b
Katysha94 <Vasilevakata76@gmail.com>
2023-08-26 21:22:08 +0300

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+player.on('timeupdate', throttle(e => {
+    localStorage.setItem("videoplayer-current-time", e.seconds);
+}, 1000));
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).catch(function (error) {
+    console.log("Error!")
+});
 
