408e5f43bd38e3f76c1870fb7dfb77fcc446fd97
Katysha94 <Vasilevakata76@gmail.com>
2023-08-27 10:06:54 +0300

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(".feedback-form");
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const LOCAL_KEY = "feedback-form-state";
+let data = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+
+if (data) {
+    for (let key in data) {
+        form[key].value = data[key]
+    }
+}
+
+form.addEventListener('input', throttle(saveData, 500));
+form.addEventListener('submit', onSubmit)
+
+function saveData(event) {
+    const { name, value } = event.target;
+    data[name] = value;
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(data))
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.log({ email: email.value, message: message.value });
+    if (email.value === '' || message.value === '') {
+        return alert('All fields must be filled!');
+    }
+    localStorage.removeItem(LOCAL_KEY);
+    event.currentTarget.reset();
+    data = {};
+}
